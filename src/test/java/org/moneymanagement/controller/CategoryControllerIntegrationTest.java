5ec098ae416fb2e55570bb6ad5415e1f25cfb3d6
Rishabh kumar <princekumar987@icloud.com>
2026-08-25 22:28:47 +0530

    checking

--- src/test/java/org/moneymanagement/controller/CategoryControllerIntegrationTest.java
+++ src/test/java/org/moneymanagement/controller/CategoryControllerIntegrationTest.java
@@ -10,7 +10,7 @@ import org.moneymanagement.service.impl.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,7 +32,7 @@ class CategoryControllerIntegrationTest {
     @Autowired
     private JwtUtils jwtUtils;
 
-    @MockBean
+    @MockitoBean
     private EmailService emailService;
 
     private String jwtToken;
